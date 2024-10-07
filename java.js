6072d6252dc1b021d9c341e99ab84f7f007dda42
rokkjaa <karinamerchan664@gmail.com>
2024-10-07 16:28:21 -0500

    .

---
+++ java.js
@@ -0,0 +1,11 @@
+const container = document.getElementsById('container');
+const registrerBtn = document.getElementsById('register');
+const loginBtn = document.getElementsById('login');
+
+registrerBtn.addEventListener('click',() =>{
+    container.classList.add("active");
+});
+loginBtn.addEventListener('click',() =>{
+    container.classList.remove("active");
+});
+<link rel="stylesheet" href="login.css">
